725f81a8244faa1aaf3082d1a6a0d5fd87a31201
teabyii <teabyii@gmail.com>
2018-04-01 22:17:28 +0800

    添加 basic 的例子

---
+++ basic/src/index.js
@@ -0,0 +1,5 @@
+require('./style.less')
+const { log } = require('./utils')
+
+log('hello world')
+
---
+++ basic/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        use: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的 loader
+        use: ExtractTextPlugin.extract({ 
+          fallback: 'style-loader',
+          use: [
+            'css-loader', 
+            'less-loader',
+          ],
+        }), 
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
+    ],
+  },
+
+  // 代码模块路径解析的配置
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, 'src'),
+    ],
+
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 配置输出文件名和路径
+      template: 'src/index.html', // 配置文件模板
+    }),
+    new ExtractTextPlugin('[name].css'),
+  ],
+}
